71d9a51f484a44ad885e5f195312a0a88d61c182
Tanmay Arya <tanmayarya2018@gmail.com>
2026-08-10 03:07:25 +0530

    remove active tests and replace Tokenize with boilerplate

---
+++ lexer/lexer.cc
@@ -0,0 +1,15 @@
+#include "lexer.hh"
+#include <cctype>
+#include "../types/token/all.hh"
+
+namespace baka {
+namespace lexer {
+
+
+std::vector<types::Token> Tokenize(std::string_view SourceCode) {
+    std::vector<types::Token> tokens;
+    return tokens;
+}
+
+} // namespace lexer
+} // namespace baka
