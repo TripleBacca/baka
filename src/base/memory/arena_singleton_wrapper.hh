ebfda37d48eb8c5ac662da9b8c2ea1d423906026
Tanmay Arya <tanmayarya2018@gmail.com>
2026-08-24 05:55:45 +0530

    implement singleton wrapper for arenas

---
+++ src/base/memory/arena_singleton_wrapper.hh
@@ -0,0 +1,28 @@
+#pragma once
+
+
+namespace baka {
+namespace base {
+
+
+// enumerate all kinds of different arenas we need
+enum class AllocArenaTag {
+    SL_STRING, // one for string literals
+    AST_NODE
+};
+
+
+template<class Arena, AllocArenaTag TAG_>
+class SingletonArena : public Arena {
+    private:
+        SingletonArena() = default;
+
+    public:
+        static SingletonArena& getInstance() {
+            static SingletonArena instance;
+            return instance;
+        }
+};
+
+}
+}
