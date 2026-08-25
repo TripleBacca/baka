c8446b33ad5c87c4f4775422a0bedfd9b9e1acb7
Satindra Ramalingam <satindra.r@gmail.com>
2026-08-25 21:34:59 +0530

    change Variable and Function names to IdentifierNode*s

--- src/types/parser/ast/struct.hh
+++ src/types/parser/ast/struct.hh
@@ -10,10 +10,10 @@ namespace baka {
     namespace types{
         class StructDeclarationStatementNode : public StatementNode {
             std::string_view DataType;
-            std::string_view VariableName;
+            IdentiferNode* VariableName;
 
         public:
-            StructDeclarationStatementNode(std::string_view dataType, std::string_view variableName) :
+            StructDeclarationStatementNode(std::string_view dataType, IdentiferNode* variableName) :
                 DataType(dataType), VariableName(variableName) {
             }
 
@@ -21,8 +21,7 @@ namespace baka {
                 INDENT(Tabs);
                 std::cout << "StructDeclaration(" << DataType << ", " << '\n';
 
-                INDENT(Tabs + 1);
-                std::cout << VariableName << '\n';
+                VariableName->Print(Tabs + 1);
 
                 INDENT(Tabs);
                 std::cout << ")" << std::endl;
@@ -31,11 +30,11 @@ namespace baka {
 
 
         class StructNode : public ASTNode {
-            std::string_view StructName;
+            IdentiferNode* StructName;
             std::vector<StructDeclarationStatementNode*> Body;
 
         public:
-            StructNode(std::string_view identifier,
+            StructNode(IdentiferNode* identifier,
                        std::vector<StructDeclarationStatementNode*> body) : StructName(identifier),
                                                                             Body(std::move(body)) {
             }
